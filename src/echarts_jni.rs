c3d3259ca14703c91e770f2a0f6b8490626d90cb
yangchuanlong <805114264@qq.com>
2025-05-14 14:33:01 +0800

    执行失败抛出异常，而不是直接终止程序

--- src/echarts_jni.rs
+++ src/echarts_jni.rs
@@ -4,43 +4,98 @@ use charming::{ImageFormat, ImageRenderer};
 use jni::JNIEnv;
 use jni::objects::*;
 use jni::sys::jint;
+use thiserror::Error;
+
+macro_rules! unwarp_exception {
+    ($val:expr,$env:expr,$err_val:expr) => {
+        match $val {
+            Ok(v) => v,
+            Err(e) => {
+                $env.exception_clear().expect("clear");
+                $env.throw_new("java/lang/RuntimeException", format!("{}", e))
+                    .unwrap();
+                return $err_val;
+            }
+        }
+    };
+}
+
+#[derive(Error, Debug)]
+pub enum EchartsError {
+    #[error("文件格式错误")]
+    Format,
+    #[error("未知错误，请提交issue")]
+    Unknown,
+}
 
 /// 跟进路径和原始json渲染图表
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn Java_top_magicpotato_Echarts_save(mut env: JNIEnv, _class: JClass, width: jint, height: jint, path: JString, data: JString) {
-    let data: String = env.get_string(&data).expect("Couldn't get data string!").into();
-    let path: String = env.get_string(&path).expect("Couldn't get path string!").into();
+pub unsafe extern "C" fn Java_top_magicpotato_Echarts_save(
+    mut env: JNIEnv,
+    _class: JClass,
+    width: jint,
+    height: jint,
+    path: JString,
+    data: JString,
+) {
+    let data: String = env
+        .get_string(&data)
+        .expect("Couldn't get data string!")
+        .into();
+    let path: String = env
+        .get_string(&path)
+        .expect("Couldn't get path string!")
+        .into();
     let mut renderer = ImageRenderer::new(width as u32, height as u32);
 
     let path = Path::new(&path);
     if path.extension().expect("无法获取文件扩展名") == "svg" {
-        renderer.save_by_json(data, path).unwrap();
+        unwarp_exception!(renderer.save_by_json(data, path), env, ());
     } else {
-        renderer.save_format_by_json(parse_extension_by_path(path), data, path).unwrap();
+        let extension = unwarp_exception!(parse_extension_by_path(path), env, ());
+        unwarp_exception!(renderer.save_format_by_json(extension, data, path), env, ());
     }
 }
 
 /// 跟进json渲染数据  返回byte数组
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn Java_top_magicpotato_Echarts_render<'local>(mut env: JNIEnv<'local>, _class: JClass, width: jint, height: jint, extension: JString<'local>, data: JString<'local>) -> JByteArray<'local> {
-    let data: String = env.get_string(&data).expect("Couldn't get data string!").into();
-    let extension: String = env.get_string(&extension).expect("Couldn't get path string!").into();
+pub unsafe extern "C" fn Java_top_magicpotato_Echarts_render<'local>(
+    mut env: JNIEnv<'local>,
+    _class: JClass,
+    width: jint,
+    height: jint,
+    extension: JString<'local>,
+    data: JString<'local>,
+) -> JByteArray<'local> {
+    let data: String = env
+        .get_string(&data)
+        .expect("Couldn't get data string!")
+        .into();
+    let extension: String = env
+        .get_string(&extension)
+        .expect("Couldn't get path string!")
+        .into();
     let mut renderer = ImageRenderer::new(width as u32, height as u32);
 
     if extension == "svg" {
-        let x = renderer.render_by_json(data).unwrap();
+        let x = unwarp_exception!(renderer.render_by_json(data), env, JByteArray::default());
         env.byte_array_from_slice(x.as_bytes()).unwrap()
     } else {
-        let x = renderer.render_format_by_json(parse_extension(&extension), data).unwrap();
+        let extension = unwarp_exception!(parse_extension(&extension), env, JByteArray::default());
+        let x = unwarp_exception!(
+            renderer.render_format_by_json(extension, data),
+            env,
+            JByteArray::default()
+        );
         env.byte_array_from_slice(x.as_slice()).unwrap()
     }
 }
 
 /// 通过文件名字解析扩展名
-fn parse_extension_by_path(path: &Path) -> ImageFormat {
-    ImageFormat::from_path(path).expect(&format!("不支持的扩展名"))
+fn parse_extension_by_path(path: &Path) -> Result<ImageFormat, EchartsError> {
+    ImageFormat::from_path(path).map_err(|_| EchartsError::Format)
 }
 
-fn parse_extension(ext: &str) -> ImageFormat {
-    ImageFormat::from_extension(ext).expect(&format!("不支持的扩展名"))
+fn parse_extension(ext: &str) -> Result<ImageFormat, EchartsError> {
+    ImageFormat::from_extension(ext).ok_or(EchartsError::Format)
 }
